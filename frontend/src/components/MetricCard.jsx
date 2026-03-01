cc4e74f59f4b711fd92bc8750d9da93bb300f0df
rgpari2006 <rgpari2006@gmail.com>
2026-03-01 20:33:49 +0530

    Delete frontend directory

--- frontend/src/components/MetricCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function MetricCard({ title, value, subtitle }) {
-  return (
-    <div className="card metric-card">
-      <p className="metric-title">{title}</p>
-      <h2>{value}</h2>
-      {subtitle && <small>{subtitle}</small>}
-    </div>
-  )
-}
